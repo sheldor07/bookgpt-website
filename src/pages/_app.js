5347cd72e182ed7db44530ddd26b5a1ef3a1ceac
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-03-29 15:59:16 +0800

    tryingt o fix template image issue
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,10 @@ export default function App({ Component, pageProps }) {
             <meta charset="UTF-8"
               name="viewport" 
               content="width=device-width, initial-scale=1.0">
+                
           </meta>
+          <link rel="shortcut icon" type="image/jpg"href="../../public/favicon.ico"/>
+
           
         </Head>
         <Component {...pageProps} />
--- src/pages/knowledge-hub/components/book-card.js
+++ src/pages/knowledge-hub/components/book-card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BookTemplate = ({ img, price, title, description }) => {
     return (
 <div className="bg-[#1b1b1b] shadow-black card rounded-lg  shadow-lg transform transition duration-300 hover:bg-[#3d3c3c]" style={{ cursor: 'pointer', minHeight: '700px' }}>
-            <img src={img} alt={title} className="h-full object-fit" />
+            <img src={img} alt={title} className="object-cover h-full" />
             <div className="p-4">
                 <h3 className="text-xl font-bold text-white">{title}</h3>
                 <p className="mt-2 text-gray-400">{price}</p>
