00aca55edac4b5d351e075f4d47d65647d62b6d4
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-03-29 19:40:14 +0800

    millionth attempt at fixing image resizing in mobile
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- src/pages/knowledge-hub/components/book-card.js
+++ src/pages/knowledge-hub/components/book-card.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
 const BookTemplate = ({ img, price, title, description }) => {
     return (
-<div className="bg-[#1b1b1b] shadow-black card rounded-lg  shadow-lg transform transition duration-300 hover:bg-[#3d3c3c]" style={{ cursor: 'pointer'}}>
-            <img src={img} alt={title} className="object-cover h-full" />
+        <div className="bg-[#1b1b1b] shadow-black card rounded-lg shadow-lg transform transition duration-300 hover:bg-[#3d3c3c]" style={{ cursor: 'pointer', maxHeight: '700px' }}>
+            <img src={img} alt={title} className="w-full object-cover h-[45%] md:h-[30%] lg:h-[50%]" />
             <div className="p-4">
                 <h3 className="text-xl font-bold text-white">{title}</h3>
                 <p className="mt-2 text-gray-400">{price}</p>
