d3191d7e79b36e9895c552cfc6c72918711b7345
Yajat Gulati <yajatgulati01@gmail.com>
2023-09-03 19:54:10 +0800

    mongodb connection error
    Signed-off-by: Yajat Gulati <yajatgulati01@gmail.com>

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 const options = {
@@ -6,16 +6,16 @@ const options = {
   useNewUrlParser: true,
 };
 
-let client;
 let clientPromise;
 
-if (process.env.NODE_ENV === 'production') {
-  // In production mode, we'll set up a connection that's reused across requests
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-} else {
-  // In development mode, create a new client for every request
-  clientPromise = MongoClient.connect(uri, options);
+try {
+  if (process.env.NODE_ENV === "production") {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+  } else {
+    clientPromise = MongoClient.connect(uri, options);
+  }
+} catch (error) {
+  console.error("MongoDB connection error:", error);
 }
-
 export default clientPromise;
