d03f364cd5db329d2519ae69ae3f4e438e8c5417
Yajat Gulati <yajatgulati01@gmail.com>
2023-09-05 18:01:54 +0800

    why google analytics not work
    Signed-off-by: Yajat Gulati <yajatgulati01@gmail.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,18 @@ export default function Document() {
       <body class="dark" id="body">
         <Main />
         <NextScript />
+        <div className="container">
+          <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+          <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+          </Script>
+        </div>
       </body>
     </Html>
   );
