4cd6a9d67cb83a445bcc7ad2aa1f1caab6615ea7
Yajat Gulati <yajatgulati01@gmail.com>
2023-09-05 17:58:39 +0800

    google anlytics script defer bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,38 @@
-
-import '@/styles/globals.css'
-import 'tailwindcss/tailwind.css';
-import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head'
+import "@/styles/globals.css";
+import "tailwindcss/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
-  return(
+  return (
     <>
-        <Head>
-            <title>GPT Book Club</title>
-            <meta charset="UTF-8"
-              name="viewport" 
-              content="width=device-width, initial-scale=1.0">
-                
-          </meta>
-          <link rel="shortcut icon" type="image/jpg"href="../../public/favicon.ico"/>
-
-          
-        </Head>
-        <Component {...pageProps} />
-        <Analytics />
+      <Head>
+        <title>GPT Book Club</title>
+        <meta
+          charset="UTF-8"
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <link
+          rel="shortcut icon"
+          type="image/jpg"
+          href="../../public/favicon.ico"
+        />
+      </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+      <Component {...pageProps} />
+      <Analytics />
     </>
-
-  )
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,19 +3,6 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_MEASUREMENT_ID}`}
-      />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'GA_MEASUREMENT_ID');
-        `}
-      </Script>
-
       <body class="dark" id="body">
         <Main />
         <NextScript />
