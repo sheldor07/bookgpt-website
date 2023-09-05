f7b5b37986f248a7248e3506d0097c422acac16d
Yajat Gulati <yajatgulati01@gmail.com>
2023-09-05 17:54:16 +0800

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        
-      </Head>
-      <body class="dark"id="body">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+
+      <body class="dark" id="body">
         <Main />
         <NextScript />
-        
       </body>
     </Html>
-  )
+  );
 }
