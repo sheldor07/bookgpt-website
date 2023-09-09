1c6822bf2d09222912955e740d1f652fb855f8b1
Yajat Gulati <yajatgulati01@gmail.com>
2023-09-09 14:47:52 +0800

    just put analytics out there
    Signed-off-by: Yajat Gulati <yajatgulati01@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,14 +8,14 @@ export default function App({ Component, pageProps }) {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-GM4MZES6G7`}
       ></Script>
       <Script strategy="lazyOnload">
         {`window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
   
-    gtag('config', ${process.env.GOOGLE_MEASUREMENT_ID});`}
+    gtag('config',  G-GM4MZES6G7);`}
       </Script>
       <Component {...pageProps} />
       <Analytics />
